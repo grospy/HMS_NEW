5338685cc1a3b1c7050897744d25f1dc96c858c5
gr0spy <shamilkerimli@gmail.com>
2018-12-27 16:58:12 +0400

    Changes_1
    Changes_1

--- library/edihistory/test_edih_sftp_files.php
+++ library/edihistory/test_edih_sftp_files.php
@@ -190,32 +190,7 @@ if (isset($_POST['post_sftp'])) {
 // a select list of x12 partners and submit button and process button
 // append output of edih_disp_sftp_upload() to displayed file list
 
-/**
- * Unattended SFTP host operations using phpseclib.
- *
- * Copyright (C) 2014 MD Support <mdsupport@users.sourceforge.net>
- *
- * LICENSE: This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://opensource.org/licenses/gpl-license.php>.
- *
- * @package   OpenEMR
- * @author    MD Support <mdsupport@users.sourceforge.net>
- *
- * Following parameters may be provided :
- * 1. actn - get or put
- * 2. Interface with Procedure providers : ppid
- * 3. Single host definition : host, port, user, pass, fdir, pdir
- * 4. ldir - local directory
- * 5. sub - 'ppid'/'host'/'fdir'/'pdir' to be used as subdir of ldir
- **/
+
 
 
 if (php_sapi_name() == 'cli') {
