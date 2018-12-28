bab339a1be6414c9cab5af7054b883bb67423f6b
gr0spy <shamilkerimli@gmail.com>
2018-12-28 21:06:51 +0400

    Changes made to view.php, new.php
    Changes made to view.php, new.php

--- contrib/forms/psychiatrySet/brief_aan_verwijzer/view.php
+++ contrib/forms/psychiatrySet/brief_aan_verwijzer/view.php
@@ -1,64 +1,19 @@
-<?php
-/*
- * brief_aan_verwijzer
- * letter to ... - Dutch specific form
- * Version: 1.0 - 30-03-2008
- */
-
-include_once("../../globals.php");
-include_once("$srcdir/api.inc");
-include_once("$srcdir/patient.inc");
-
-$returnurl = 'encounter_top.php';
-
-$result = getPatientData($pid, "fname,lname,pid,pubpid,phone_home,sex,pharmacy_id,DOB,DATE_FORMAT(DOB,'%Y%m%d') as DOB_YMD");
-$provider_results = sqlQuery("select * from users where username='" . $_SESSION{"authUser"} . "'");
-
-////////////////////////////////////////////////////////////////////
-// Function:	getPatientDateOfLastEncounter
-function getPatientDateOfLastEncounter($nPid)
-{
-  // get date of last encounter no codes
-    $strEventDate = sqlQuery("SELECT MAX(pc_eventDate) AS max
+<?php include_once("../../globals.php");include_once($srcdir."/api.inc");include_once($srcdir."/patient.inc");$returnurl='encounter_top.php';$result=getPatientData($pid,"fname,lname,pid,pubpid,phone_home,sex,pharmacy_id,DOB,DATE_FORMAT(DOB,'%Y%m%d') as DOB_YMD");$provider_results=sqlQuery("select * from users where username='".$_SESSION{"authUser"}."'");function getPatientDateOfLastEncounter($nPid){$strEventDate=sqlQuery("SELECT MAX(pc_eventDate) AS max
                   FROM openemr_postcalendar_events
-                  WHERE pc_pid = $nPid
+                  WHERE pc_pid = ".$nPid."
                   AND pc_apptstatus = '@'
-                  AND pc_eventDate >= '2007-01-01'");
-
-  // now check if there was a previous encounter
-    if ($strEventDate['max'] != "") {
-        return( $strEventDate['max'] );
-    } else {
-        return( "00-00-0000" );
-    }
-}
-
-$m_strEventDate = getPatientDateOfLastEncounter($result['pid']);
-
-// get autosave id for Psychiatrisch Onderzoek
-$vectAutosave = sqlQuery("SELECT id, autosave_flag, autosave_datetime FROM form_brief_aan_verwijzer
-                            WHERE pid = ".$_SESSION["pid"].
-                            " AND groupname='".$_SESSION["authProvider"].
-                            "' AND user='".$_SESSION["authUser"]."' AND
-                            authorized=$userauthorized AND activity=1
+                  AND pc_eventDate >= '2007-01-01'");if($strEventDate['max']!=""){return($strEventDate['max']);}else{return("00-00-0000");}}$m_strEventDate=getPatientDateOfLastEncounter($result['pid']);$vectAutosave=sqlQuery("SELECT id, autosave_flag, autosave_datetime FROM form_brief_aan_verwijzer
+                            WHERE pid = ".$_SESSION["pid"]." AND groupname='".$_SESSION["authProvider"]."' AND user='".$_SESSION["authUser"]."' AND
+                            authorized=".$userauthorized." AND activity=1
                             AND autosave_flag=1
-                            ORDER by id DESC limit 1");
-
-if ($vectAutosave['id'] && $vectAutosave['id'] != "" && $vectAutosave['id'] > 0) {
-    $obj = formFetch("form_brief_aan_verwijzer", $vectAutosave['id']);
-} else {
-    $obj = formFetch("form_brief_aan_verwijzer", (int)$_GET["id"]);
-}
-
-?>
-
+                            ORDER by id DESC limit 1");if($vectAutosave['id']&&$vectAutosave['id']!=""&&$vectAutosave['id']>0){$obj=formFetch("form_brief_aan_verwijzer",$vectAutosave['id']);}else{$obj=formFetch("form_brief_aan_verwijzer",(int)$_GET["id"]);};echo'
 <html>
     <head>
-        <link rel=stylesheet href="<?php echo $css_header;?>" type="text/css">
-        <link rel="stylesheet" href="<?php echo $GLOBALS['assets_static_relative']; ?>/jquery-datetimepicker-2-5-4/build/jquery.datetimepicker.min.css">
+        <link rel=stylesheet href="';echo$css_header;echo'" type="text/css">
+        <link rel="stylesheet" href="';echo$GLOBALS['assets_static_relative'];echo'/jquery-datetimepicker-2-5-4/build/jquery.datetimepicker.min.css">
     </head>
 
-<body <?php echo $top_bg_line;?> topmargin=0 rightmargin=0 leftmargin=2 bottommargin=0 marginwidth=2 marginheight=0>
+<body ';echo$top_bg_line;echo' topmargin=0 rightmargin=0 leftmargin=2 bottommargin=0 marginwidth=2 marginheight=0>
 
 <style type="text/css">
  body       { font-family:sans-serif; font-size:10pt; font-weight:normal }
@@ -68,42 +23,27 @@ if ($vectAutosave['id'] && $vectAutosave['id'] != "" && $vectAutosave['id'] > 0)
                                padding-left:3px; padding-right:3px; }
 </style>
 
-<script type="text/javascript" src="../../../library/dialog.js?v=<?php echo $v_js_includes; ?>"></script>
-<script type="text/javascript" src="../../../library/textformat.js?v=<?php echo $v_js_includes; ?>"></script>
-<script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/jquery-min-3-1-1/index.js"></script>
-<script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/jquery-datetimepicker-2-5-4/build/jquery.datetimepicker.full.min.js"></script>
+<script type="text/javascript" src="../../../library/dialog.js?v=';echo$v_js_includes;echo'"></script>
+<script type="text/javascript" src="../../../library/textformat.js?v=';echo$v_js_includes;echo'"></script>
+<script type="text/javascript" src="';echo$GLOBALS['assets_static_relative'];echo'/jquery-min-3-1-1/index.js"></script>
+<script type="text/javascript" src="';echo$GLOBALS['assets_static_relative'];echo'/jquery-datetimepicker-2-5-4/build/jquery.datetimepicker.full.min.js"></script>
 
-<?php
-
-if ($_GET["id"]) {
-    $brief_aan_verwijzer_id = $_GET["id"];
-} else {
-    $brief_aan_verwijzer_id = "0";
-}
-
-?>
-<script type="text/javascript">
+';if($_GET["id"]){$brief_aan_verwijzer_id=$_GET["id"];}else{$brief_aan_verwijzer_id="0";};echo'<script type="text/javascript">
 $(document).ready(function(){
         autosave();
-        $('.datepicker').datetimepicker({
-            <?php $datetimepicker_timepicker = false; ?>
-            <?php $datetimepicker_showseconds = false; ?>
-            <?php $datetimepicker_formatInput = false; ?>
-            <?php require($GLOBALS['srcdir'] . '/js/xl/jquery-datetimepicker-2-5-4.js.php'); ?>
-            <?php // can add any additional javascript settings to datetimepicker here; need to prepend first setting with a comma ?>
-        });
+        $(\'.datepicker\').datetimepicker({
+            ';$datetimepicker_timepicker=false;echo'            ';$datetimepicker_showseconds=false;echo'            ';$datetimepicker_formatInput=false;echo'            ';require($GLOBALS['srcdir'].'/js/xl/jquery-datetimepicker-2-5-4.js.php');echo'            ';;echo'        });
                         });
 
 function delete_autosave( )
 {
-  if( confirm("<?php xl('Are you sure you want to completely remove this form?', 'e'); ?>") )
+  if( confirm("';xl('Are you sure you want to completely remove this form?','e');echo'") )
   {
     $.ajax(
             {
               type: "POST",
               url: "../../forms/brief_aan_verwijzer/delete_autosave.php",
-              data: "id=" + <?php echo $brief_aan_verwijzer_id ?>
-                        ,
+              data: "id=" + ';echo$brief_aan_verwijzer_id;echo'                        ,
                                 cache: false,
                                 success: function( message )
                 {
@@ -136,7 +76,7 @@ function autosave( )
             {
               type: "POST",
               url: "../../forms/brief_aan_verwijzer/autosave.php",
-              data: "id=" + <?php echo $brief_aan_verwijzer_id ?> +
+              data: "id=" + ';echo$brief_aan_verwijzer_id;echo' +
                         "&introductie=" + $("#introductie").val() +
                         "&reden_van_aanmelding=" + a_reden_van_aanmelding +
                         "&anamnese=" + a_anamnese +
@@ -157,43 +97,34 @@ function autosave( )
 
 </script>
 
-<?php
-include_once("$srcdir/api.inc");
-//$obj = formFetch("form_brief_aan_verwijzer", (int)$_GET["id"]);
-?>
+';include_once($srcdir."/api.inc");echo'
+<form method=post action="';echo$rootdir;echo'/forms/brief_aan_verwijzer/save.php?mode=update&id=';echo$_GET["id"];echo'" name="my_form">
+<span class="title">';xl('Psychiatric Brief Letter','e');echo'</span><Br><br>
 
-<form method=post action="<?php echo $rootdir?>/forms/brief_aan_verwijzer/save.php?mode=update&id=<?php echo $_GET["id"];?>" name="my_form">
-<span class="title"><?php xl('Psychiatric Brief Letter', 'e'); ?></span><Br><br>
+<br><span class=text>';xl('Introduction','e');echo'</span><br>
+<textarea cols=80 rows=5 wrap=virtual name="introductie" id="introductie">';echo stripslashes($obj{"introductie"});echo'</textarea><br>
 
-<br><span class=text><?php xl('Introduction', 'e'); ?></span><br>
-<textarea cols=80 rows=5 wrap=virtual name="introductie" id="introductie"><?php echo stripslashes($obj{"introductie"});?></textarea><br>
-
-<br><span class=text><?php xl('Reason for Visit', 'e'); ?></span><br>
-<textarea cols=80 rows=5 wrap=virtual name="reden_van_aanmelding" id="reden_van_aanmelding"><?php echo stripslashes($obj{"reden_van_aanmelding"});?></textarea><br>
-<br><span class=text><?php xl('History', 'e'); ?></span><br>
-<textarea cols=80 rows=5 wrap=virtual name="anamnese" id="anamnese"><?php echo stripslashes($obj{"anamnese"});?></textarea><br>
-<br><span class=text><?php xl('Psychiatric Examination', 'e'); ?></span><br>
-<textarea cols=80 rows=5 wrap=virtual name="psychiatrisch_onderzoek" id="psychiatrisch_onderzoek"><?php echo stripslashes($obj{"psychiatrisch_onderzoek"});?></textarea><br>
-<br><span class=text><?php xl('Conclusions', 'e'); ?></span><br>
-<textarea cols=80 rows=5 wrap=virtual name="beschrijvend_conclusie" id="beschrijvend_conclusie"><?php echo stripslashes($obj{"beschrijvend_conclusie"});?></textarea><br>
-<br><span class=text><?php xl('Treatment Plan', 'e'); ?></span><br>
-<textarea cols=80 rows=5 wrap=virtual name="advies_beleid" id="advies_beleid"><?php echo stripslashes($obj{"advies_beleid"});?></textarea><br>
+<br><span class=text>';xl('Reason for Visit','e');echo'</span><br>
+<textarea cols=80 rows=5 wrap=virtual name="reden_van_aanmelding" id="reden_van_aanmelding">';echo stripslashes($obj{"reden_van_aanmelding"});echo'</textarea><br>
+<br><span class=text>';xl('History','e');echo'</span><br>
+<textarea cols=80 rows=5 wrap=virtual name="anamnese" id="anamnese">';echo stripslashes($obj{"anamnese"});echo'</textarea><br>
+<br><span class=text>';xl('Psychiatric Examination','e');echo'</span><br>
+<textarea cols=80 rows=5 wrap=virtual name="psychiatrisch_onderzoek" id="psychiatrisch_onderzoek">';echo stripslashes($obj{"psychiatrisch_onderzoek"});echo'</textarea><br>
+<br><span class=text>';xl('Conclusions','e');echo'</span><br>
+<textarea cols=80 rows=5 wrap=virtual name="beschrijvend_conclusie" id="beschrijvend_conclusie">';echo stripslashes($obj{"beschrijvend_conclusie"});echo'</textarea><br>
+<br><span class=text>';xl('Treatment Plan','e');echo'</span><br>
+<textarea cols=80 rows=5 wrap=virtual name="advies_beleid" id="advies_beleid">';echo stripslashes($obj{"advies_beleid"});echo'</textarea><br>
 
 <table><tr>
-<?php
-// this to be used/moved above for form header with patient name/etc
-?>
-</tr></table>
+';;echo'</tr></table>
 
 <br><br>
-<a href="javascript:document.my_form.submit();" class="link_submit">[<?php xl('Save', 'e'); ?>]</a>
+<a href="javascript:document.my_form.submit();" class="link_submit">[';xl('Save','e');echo']</a>
 <br>
-<a href="<?php echo $GLOBALS['form_exit_url']; ?>" class="link_submit"
- onclick="delete_autosave();top.restoreSession()">[<?php xl('Don\'t Save Changes', 'e'); ?>]</a>
+<a href="';echo$GLOBALS['form_exit_url'];echo'" class="link_submit"
+ onclick="delete_autosave();top.restoreSession()">[';xl('Don\'t Save Changes','e');echo']</a>
 </form>
 
 <div id="timestamp"></div>
 
-<?php
-formFooter();
-?>
+';formFooter();
